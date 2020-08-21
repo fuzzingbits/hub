f882bfa0658cbd78aea74a3119b63fa9905701d2
Aaron Ellington <aaron@ellington.io>
2020-08-21 13:47:55 -0400

    about page clean up

--- ui/pages/about.vue
+++ ui/pages/about.vue
@@ -6,9 +6,5 @@
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({
-	mounted() {
-		console.log("This is the about page.");
-	},
-});
+export default Vue.extend({});
 </script>
