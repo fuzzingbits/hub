50d8d6d5a3570605c7fe8bde38a15e8945fbf52f
Aaron Ellington <aaron@ellington.io>
2020-08-22 11:20:58 -0400

    add missing comments

--- pkg/provider/planner/provider.go
+++ pkg/provider/planner/provider.go
@@ -2,9 +2,14 @@ package plan
 
 import "github.com/fuzzingbits/hub/pkg/entity"
 
+// Provider is a Planner data provider
 type Provider interface {
+	// Get a planner by user and date
 	Get(userUUID string, date string) (entity.Planner, error)
+	// Save a planner by creating it or updating an existing one
 	Save(plan entity.Planner) error
+	// Delete an existing planner
 	Delete(plan entity.Planner) error
+	// GetAll planners for a given user
 	GetAll(userUUID string) ([]entity.Planner, error)
 }
