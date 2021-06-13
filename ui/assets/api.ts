dfbfc88288f5ad04ae4aa6afdcd426ae1d9f13e0
Aaron Ellington <aaron@ellington.io>
2021-06-13 12:19:19 -0500

    eslint config change

--- ui/assets/api.ts
+++ ui/assets/api.ts
@@ -28,47 +28,47 @@ class HubAPI {
 	// ---- Auto Generated Functions BEGIN ---- //
 
 	public async serverStatus(): Promise<GenericResponse<types.ServerStatus | null>> {
-		const response = await client.get("/api/server/status");
+		const response = await client.get('/api/server/status');
 		return response.data;
 	}
 
 	public async serverSetup(payload: types.UserCreateRequest): Promise<GenericResponse<types.UserContext | null>> {
-		const response = await client.post("/api/server/setup", payload);
+		const response = await client.post('/api/server/setup', payload);
 		return response.data;
 	}
 
 	public async userNew(payload: types.UserCreateRequest): Promise<GenericResponse<types.UserContext | null>> {
-		const response = await client.post("/api/user/new", payload);
+		const response = await client.post('/api/user/new', payload);
 		return response.data;
 	}
 
 	public async userLogin(payload: types.UserLoginRequest): Promise<GenericResponse<types.UserContext | null>> {
-		const response = await client.post("/api/user/login", payload);
+		const response = await client.post('/api/user/login', payload);
 		return response.data;
 	}
 
 	public async userLogout(): Promise<GenericResponse<null>> {
-		const response = await client.get("/api/user/logout");
+		const response = await client.get('/api/user/logout');
 		return response.data;
 	}
 
 	public async userMe(): Promise<GenericResponse<types.UserContext | null>> {
-		const response = await client.get("/api/user/me");
+		const response = await client.get('/api/user/me');
 		return response.data;
 	}
 
 	public async userList(): Promise<GenericResponse<types.User[] | null>> {
-		const response = await client.get("/api/user/list");
+		const response = await client.get('/api/user/list');
 		return response.data;
 	}
 
 	public async userDelete(): Promise<GenericResponse<null>> {
-		const response = await client.get("/api/user/delete");
+		const response = await client.get('/api/user/delete');
 		return response.data;
 	}
 
 	public async userUpdate(payload: types.UserUpdateRequest): Promise<GenericResponse<types.UserContext | null>> {
-		const response = await client.post("/api/user/update", payload);
+		const response = await client.post('/api/user/update', payload);
 		return response.data;
 	}
 
